06b6bb4f9d7fd39146bb597e8fa11dba83c00eae
MihirHundiwala <mihir.hundiwala@somaiya.edu>
2022-06-16 02:44:46 +0530

    .

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:MihirHundiwala/to-do-list-manager.git master:gh-pages
+git push -f https://github.com/MihirHundiwala/to-do-list-manager.git master:gh-pages
 cd -
