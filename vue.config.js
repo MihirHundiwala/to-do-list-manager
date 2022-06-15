f93b771e5ac3faa970274098b19073047c5f2c84
MihirHundiwala <mihir.hundiwala@somaiya.edu>
2022-06-16 04:20:50 +0530

    .

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,3 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   publicPath: process.env.NODE_ENV === 'production' ? '/to-do-list-manager/' : '/',
-//   transpileDependencies: [
-//     'vuetify'
-//   ]
-// })
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/to-do-list-manager/'
